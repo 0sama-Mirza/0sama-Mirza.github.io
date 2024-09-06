6b487c381c661aaf2fa35b7e754c1544e47b8f6c
0sama-Mirza <osama.anees789@gmail.com>
2024-09-06 22:08:10 +0500

    Added Namaz Timings

--- Script/menu.js
+++ Script/menu.js
@@ -1,6 +1,6 @@
 // menu.js
-document.addEventListener("DOMContentLoaded", function() {
-    const menu = document.querySelector('.menu');
+document.addEventListener("DOMContentLoaded", async function() {
+	const menu = document.querySelector('.menu');
     if (menu) {
         // Get the current file name
         const currentFile = window.location.pathname.split('/').pop();
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
         menu.innerHTML = `
             <section>
-                <h3 class="listhead">Explore!</h3>
+				<h3 class="listhead" style="margin-top: 15px;">Explore!</h3>
                 <nav>
                     <ul>
                         <li style="list-style-type: '🏠 ';"><a class="menuitem" href="${pathPrefix}index.html">Home</a></li>
@@ -25,20 +25,56 @@ document.addEventListener("DOMContentLoaded", function() {
                     <a style="margin-left: 7%;" href="https://decolonizepalestine.com/" target="_blank"><img alt="88x31 button that says Stand with Palestine" src="${pathPrefix}images/Buttons/standwithFalasteen.png"></a>
                 </nav>
             </section>
+            <section class="prayer-timing">
+					<h3 class="listhead" id="Salah-Time">Salah Timings</h3>
+					<!-- Drop Down Menu -->
+					<select name="countries" id="countries">
+						<option value="default" disabled selected>Loading Countries</option>
+					</select>
+					<!-- Prayer Timings as a List -->
+					<ul class="prayer-list">
+						<li class="prayer-item">
+						  <div class="prayer-details">
+							<span class="prayer-name">Fajr</span>
+							<span class="prayer-time">Loading...</span>
+						  </div>
+						</li>
+						<li class="prayer-item">
+						  <div class="prayer-details">
+							<span class="prayer-name">Dhuhr</span>
+							<span class="prayer-time">Loading...</span>
+						  </div>
+						</li>
+						<li class="prayer-item">
+						  <div class="prayer-details">
+							<span class="prayer-name">Asr</span>
+							<span class="prayer-time">Loading...</span>
+						  </div>
+						</li>
+						<li class="prayer-item">
+						  <div class="prayer-details">
+							<span class="prayer-name">Maghrib</span>
+							<span class="prayer-time">Loading...</span>
+						  </div>
+						</li>
+						<li class="prayer-item">
+						  <div class="prayer-details">
+							<span class="prayer-name">Isha</span>
+							<span class="prayer-time">Loading...</span>
+						  </div>
+						</li>
+					  </ul>					  
+			</section>
             <section>
                 <h3 class="listhead">Interesting Stuff On The Internet</h3>
                 <nav>
                     <ul>
                         <li><a class="menuitem" href="https://bugswriter.com/">BugsWriter</a></li>
-                        <li><a class="menuitem" href="https://sadgrl.online/">SadGrl</a></li>
-                        <li><a class="menuitem" href="https://links.yesterweb.org/">When The Internet Felt Exciting And Mysterious</a></li>
                         <li><a class="menuitem" href="https://adam.omg.lol/">OMG LOL</a></li>
                         <li><a class="menuitem" href="http://txti.es/">Simple Website Builder</a></li>
-                        <li><a class="menuitem" href="https://wiby.me/">Better Google</a></li>
                         <li><a class="menuitem" href="https://asnan111.github.io/CAED-Lab-1/Asnan.html">The Best Website On This Planet!</a></li>
                         <li><a class="menuitem" href="https://www.anothersadtrombone.com/?gdpr=1">Click Here If You Accomplished Something Great!</a></li>
                         <li><a class="menuitem" href="https://www.szynalski.com/tone-generator/">Cool Wave Sounds</a></li>
-                        <li><a class="menuitem" href="https://www.boredbutton.com/">Are You Bored? Click Here!</a></li>
                         <li><a class="menuitem" href="https://billsworld.neocities.org/">Time Travel To 1999!</a></li>
                         <li><a class="menuitem" href="https://pixels.crd.co/">Pixels</a></li>
                     </ul>
@@ -46,4 +82,24 @@ document.addEventListener("DOMContentLoaded", function() {
             </section>
         `;
     }
+
+		const footer = document.querySelector('footer');
+		if (footer) {
+			footer.insertAdjacentHTML('beforebegin', `
+			<div id="mapContainer">
+				<div id="map" anchor="closeMapButton"></div>
+				<button id="closeMapButton" class="close-button">&times;</button>
+			</div>
+			`);
+		} else {
+			console.log("Footer not found.");
+		}
+
+	    // Dispatch custom event after menu is fully loaded
+		const event = new Event('menuLoaded');
+		document.dispatchEvent(event);
 });
+
+
+
+
---
+++ Script/namaz-timing.js
@@ -0,0 +1,212 @@
+"use strick";
+
+document.addEventListener('menuLoaded', function() {
+  const countries = document.getElementById("countries");
+  const closeMapButton = document.getElementById("closeMapButton");
+  const Salah_Time = document.getElementById("Salah-Time");
+  const prayer_list = document.querySelector(".prayer-list");
+  let countriesData;
+  let selectedLocation;
+  let currentMarker;
+  let map;
+  const zoomLevel = 6;
+  
+  const getCurrentDate = function () {
+    const today = new Date();
+    const day = String(today.getDate()).padStart(2, "0");
+    const month = String(today.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+    const year = today.getFullYear();
+  
+    return `${day}-${month}-${year}`;
+  };
+  
+  const getJSON = async function (url, errorMsg = "Something went wrong.") {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`${errorMsg}: ${response.status}`);
+    return await response.json();
+  };
+  
+  
+  const getSensableSubRegion = function () {
+    if (!selectedLocation || !selectedLocation.address) {
+        console.log("Location data not available");
+        return null;
+    }
+    
+    const address = selectedLocation.address;
+    
+    // Prioritize the available sub-region in a sensible order
+    if (address.village) {
+        return address.village;
+    } else if (address.suburb) {
+        return address.suburb;
+    } else if (address.town) {
+        return address.town;
+    } else if (address.city_district) {
+        return address.city_district;
+    } else if (address.city) {
+        return address.city;
+    } else if (address.county) {
+        return address.county;
+    } else if (address.state) {
+        return address.state;
+    } else if (address.country) {
+        return address.country;
+    } else {
+        console.log("No sub-region found");
+        return null;
+    }
+};
+
+  
+  const Namaz_Timing = async function (lat, lng) {
+    try {
+      const Namaz_Obj = await getJSON(
+        `https://api.aladhan.com/v1/timings/${getCurrentDate()}?latitude=${lat}&longitude=${lng}&method=4&school=1&adjustment=1`,
+        "Salah Timing Not Found!"
+      );
+  
+      // Reverse GeoCoding:
+  
+      selectedLocation = await getJSON(
+        `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json`,
+        "No response from the reverse geocoding API."
+      );
+  // LocalStorage
+      const selectedLocationString = JSON.stringify(selectedLocation);
+      localStorage.setItem('selectedLocation', selectedLocationString);
+  
+      // Extract the country code
+      const countryCode = selectedLocation.address.country_code.toLowerCase();
+  
+      // Find the matching country
+      const matchingCountry = countriesData.find(country => country.altSpellings[0].toLowerCase() === countryCode);
+  
+      if (matchingCountry) {
+      } else {
+          console.error("No match found.");
+      }
+  
+      renderNamazTimings(Namaz_Obj.data.timings,matchingCountry);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  const renderNamazTimings = function (Namaz_times,matchingCountry) {
+    Salah_Time.textContent = `${matchingCountry.flag} ${getSensableSubRegion()} ${selectedLocation.address.country}`;
+    prayer_list.innerHTML = `
+              <li class="prayer-item">
+                <span class="prayer-name">Fajr</span>
+                <span class="prayer-time">${Namaz_times.Fajr}</span>
+              </li>
+              <li class="prayer-item">
+                <span class="prayer-name">Dhuhr</span>
+                <span class="prayer-time">${Namaz_times.Dhuhr}</span>
+              </li>
+              <li class="prayer-item">
+                <span class="prayer-name">Asr</span>
+                <span class="prayer-time">${Namaz_times.Asr}</span>
+              </li>
+              <li class="prayer-item">
+                <span class="prayer-name">Maghrib</span>
+                <span class="prayer-time">${Namaz_times.Maghrib}</span>
+              </li>
+              <li class="prayer-item">
+                <span class="prayer-name">Isha</span>
+                <span class="prayer-time">${Namaz_times.Isha}</span>
+              </li>
+    `;
+  };
+  
+  const showNamazTiming = function () {
+    document.getElementById("map").style.display = "none";
+    closeMapButton.style.display = "none";
+    countries.value = "default";
+    prayer_list.style.display = "flex";
+  };
+  
+  (async function () {
+    try {
+      const storageKey = "countriesData";
+      countriesData = localStorage.getItem(storageKey);
+      if (countriesData) {
+        countriesData = JSON.parse(countriesData);
+        const savedLocationString = localStorage.getItem('selectedLocation');
+        if(savedLocationString){
+          const savedLocation = JSON.parse(savedLocationString);
+          Namaz_Timing(savedLocation.lat,savedLocation.lon);
+          showNamazTiming();
+        }
+      } else {
+        countriesData = await getJSON("https://restcountries.com/v3.1/all");
+        localStorage.setItem(storageKey, JSON.stringify(countriesData));
+      }
+      let outputCountries =
+        '<option value="default" disabled selected>Select a country</option>';
+      countriesData.forEach((country, index) => {
+        let countryName = country.name.common;
+        if (country.name.common === "Israel")
+          countryName = "💩Terrorist Dog Madarchod IsNotReal💩";
+        outputCountries += `<option value=${index}>${countryName} ${country.flag}</option>`;
+      });
+      countries.innerHTML = outputCountries;
+      // Load MAP:
+      map = L.map("map").setView([51.505, -0.09], zoomLevel);
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
+  
+      map.on("click", (mapE) => {
+        mapEvent = mapE;
+        if (currentMarker) {
+          map.removeLayer(currentMarker);
+        }
+        currentMarker = L.marker(mapEvent.latlng)
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              autoClose: false,
+              closeOnClick: false,
+            })
+          )
+          .setPopupContent(
+            `
+Location Set! You Can Close Now.
+  `
+          )
+          .openPopup();
+        const { lat } = currentMarker._latlng;
+        const { lng } = currentMarker._latlng;
+        Namaz_Timing(lat, lng);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  })();
+  
+  const movMap = function (latlng) {
+    map.setView(latlng, zoomLevel, {
+      animate: true,
+      pan: { duration: 1 },
+    });
+  };
+  
+  countries.addEventListener("click", function () {
+    if (this.value === "default" || !this.value) return;
+    selectedLocation = countriesData[+this.value];
+    movMap(selectedLocation.latlng);
+    document.getElementById("map").style.display = "block";
+    closeMapButton.style.display = "block";
+  });
+  
+  closeMapButton.addEventListener("click", function () {
+    document.getElementById("map").style.display = "none";
+    closeMapButton.style.display = "none";
+    countries.value = "default";
+  });
+  
+})
+
